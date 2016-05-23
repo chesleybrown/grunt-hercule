63eb080b9f58889d7ebbffc15da05a61439502ca
Jun Matsushita <jun@iilab.org>
2016-05-23 15:48:42 +0200

    Updated hercule and deal with mutliple files

--- tasks/hercule.js
+++ tasks/hercule.js
@@ -1,31 +1,37 @@
 'use strict';
 
 var hercule = require('hercule');
+var path = require('path')
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('hercule', function () {
 		var done = this.async();
-		
-		if (this.filesSrc.length != 1) {
-			grunt.fail.fatal('You must provide only one src file.');
-		}
-		
-		var src = this.filesSrc[0];
-		var dest = this.data.dest;
-		
-		hercule.transcludeFile(src,
-			function logger(msg) {
-				grunt.verbose.writeln(msg);
-			},
-			function callback(doc) {
-				grunt.file.write(dest, doc);
-				
-				grunt.log.write(src);
-				grunt.log.write(' >> '.green);
-				grunt.log.writeln(dest);
-				
-				done();
-			}
-		);
+		var num_files = this.files.length
+		this.files.forEach(function(f){
+			hercule.transcludeFile(f.src[0],
+				{ relativePath: path.dirname(path.join(process.cwd(), f.src[0])) },
+				function callback(err,output) {
+
+					if (err) {
+						console.log(err)
+						console.log(path.join(process.cwd(), f.src[0]))
+						console.log(path.dirname(path.join(process.cwd(), f.src[0])));
+					} else {
+						grunt.file.write(path.join(process.cwd(), f.dest), output);
+					}
+
+					num_files = num_files - 1
+
+					// grunt.log.write(f.src[0]);
+					// grunt.log.write(' >> '.green);
+					// grunt.log.writeln(f.dest);
+
+					if (num_files == 0) done();
+
+
+				}
+			);
+		});
+
 	});
 };
