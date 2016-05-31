bf0a3d853d88f403eaa3e4109cfc5c70492c31e1
Jun Matsushita <jun@iilab.org>
2016-05-31 18:34:43 +0200

    Fixing coding style

--- tasks/hercule.js
+++ tasks/hercule.js
@@ -1,33 +1,29 @@
 'use strict';
 
 var hercule = require('hercule');
-var path = require('path')
-var fs = require('fs')
+var path = require('path');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('hercule', function () {
 		var done = this.async();
-		var num_files = this.files.length
-
-		this.files.forEach(function(f){
+		var numFiles = this.files.length;
+		this.files.forEach(function (f) {
 			hercule.transcludeFile(f.src[0],
-				{ relativePath: path.dirname(path.join(process.cwd(), f.src[0])) },
-				function callback(err,output) {
-
+				{relativePath: path.dirname(path.join(process.cwd(), f.src[0]))},
+				function callback(err, output) {
 					if (err) {
-						grunt.log.error("Hercule error while processing: " + f.src[0])
-						grunt.log.verbose.error(err)
-					} else {
+						grunt.log.error('Hercule error while processing: ' + f.src[0]);
+						grunt.log.verbose.error(err);
+					}
+					else {
 						grunt.file.write(path.join(process.cwd(), f.dest), output);
 					}
-
-					num_files = num_files - 1
-
-					if (num_files == 0) done();
-
+					numFiles = numFiles - 1;
+					if (numFiles === 0) {
+						done();
+					}
 				}
 			);
 		});
-
 	});
 };
